99c2358ffc164c69151879a9dff7c3c0d32b5176
Mikael Jorhult <mikael@jorhult.se>
2016-03-06 11:11:27 +0100

    Simplify output function.

--- RiverReader.php
+++ RiverReader.php
@@ -33,44 +33,32 @@ class RiverReader {
 	 *
 	 */
 	public function displayRiver() {
-		
 		$html = '';
-		
-		foreach( $this->river->updatedFeeds as $feeds ) {
-			
-			$html .= '<ul class="river-feeds">';
-			
-			foreach( $feeds as $feed ) {
-				
+
+		foreach ( $this->river->updatedFeeds as $feeds ) {
+			foreach ( $feeds as $feed ) {
 				foreach ( $feed->item as $item ) {
-				
 					$html .= '<li class="item">';
-					
+
 					if ( $item->title != '' ) {
 						$html .= '<h2><a href="' . $item->link . '">' . $item->title . '</a></h2>';
 					}
-					
+
 					$html .= '<h3>' . $feed->feedTitle . ' (<a href="' . $feed->websiteUrl . '">website</a>)</h3>';
-					
-					
+
+
 					if ( $item->body != '' ) {
 						$html .= '<div class="item-body">' . $item->body . '</div>';
 					}
-					
+
 					$html .= '<div class="metadata">' . $item->pubDate . ' – <a href="' . $item->link . '">Permalink</a></div>';
-					
+
 					$html .= '</li> <!-- .item -->';
-				
 				}
-				
 			}
-			
-			$html .= '</ul> <!-- .feeds -->';
-			
 		}
-		
-		return $html;
-		
+
+		return '<ul class="river-feeds">' . $html . '</ul> <!-- .feeds -->';
 	}
 
 	/**
